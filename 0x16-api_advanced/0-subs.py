de1322b369bcfc597ac48a8aa90b8579b5557c26
Abdulazizgr <abdulazizisa579@gmail.com>
2024-08-13 12:10:02 +0000

    updating o-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,26 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "test"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        
+        if response.status_code == 404:
+            return 0
+        
+        
+        results = response.json().get("data")
+        
+        
+        return results.get("subscribers", 0)
+    
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
